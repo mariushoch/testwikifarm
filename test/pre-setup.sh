d3cb8ff7d057fcc94a6e7d7fe18383a231310c32
Marius Hoch <mail@mariushoch.de>
2024-05-05 17:33:54 +0300

    test: Update to mw 0.24.0

--- test/pre-setup.sh
+++ test/pre-setup.sh
@@ -5,11 +5,8 @@ set -e
 
 cd ..
 
-curl 'https://gitlab.wikimedia.org/repos/releng/cli/-/jobs/254914/artifacts/download' > mw.zip
-unzip -j mw.zip bin/mw
-mv mw /usr/local/bin/mw
+curl 'https://gitlab.wikimedia.org/api/v4/projects/16/packages/generic/mwcli/v0.24.0/mw_v0.24.0_linux_amd64' > /usr/local/bin/mw
 chmod +x /usr/local/bin/mw
-rm mw.zip
 
 git clone --depth 1 https://github.com/wikimedia/mediawiki.git /srv/mediawiki
 cp -r testwikifarm /srv/mediawiki/extensions/

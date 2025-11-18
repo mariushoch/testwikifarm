c0407e2fd11e182e2f1504314efd766c534eae95
Marius Hoch <mail@mariushoch.de>
2025-11-18 10:34:29 +0100

    Set the elasticsearch image
    Until we have new mw cli release.

--- setup.sh
+++ setup.sh
@@ -9,8 +9,14 @@ mw docker mysql create --no-interaction
 # Create the Memcached container
 mw docker memcached create --no-interaction
 
+# Per https://phabricator.wikimedia.org/T410269#11378593
+mw docker elasticsearch image set 'docker-registry.wikimedia.org/repos/search-platform/cirrussearch-opensearch-image:v1.3.20-12'
+
 # Create the ElasticSearch container
 mw docker elasticsearch create --no-interaction
+
+# The container has been created already, so we can remove the overwrite again (and leave the state clean for further updates)
+mw docker elasticsearch image reset
 
 # Download extensions and skins
 mw docker mediawiki get-code --use-github --gerrit-interaction-type http --skin MinervaNeue || true
